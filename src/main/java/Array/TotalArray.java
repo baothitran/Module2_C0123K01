ef6c141fe5e2fa0c07687e023f98d98995826d54
baothitran <baothitran.ntp@gmail.com>
2023-03-10 17:17:21 +0700

    update

--- src/main/java/Array/TotalArray.java
+++ src/main/java/Array/TotalArray.java
@@ -2,7 +2,7 @@ package Array;
 
 public class TotalArray {
     public static void main(String[] args) {
-        int[] arr = {2,7,8,6,3,5};
+        int[] arr = {10,7,8,6,9,5};
         int total = 0;
         for(int i=0; i<arr.length; i++){
             total += arr[i];
