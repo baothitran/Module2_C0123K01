b50b97951a41be2bf2e82a0dd50da4fe6d2f286c
baothitran <baothitran.ntp@gmail.com>
2023-03-16 08:56:31 +0700

    commit

--- src/main/java/newdemo/Student8.java
+++ src/main/java/newdemo/Student8.java
@@ -1,21 +1,12 @@
 package newdemo;
 
 public class Student8 {
-    int rollno;
-    String name;
-    static String college = "Bưu chính viễn thông";
-
-    Student8(int r, String n) {
-        rollno = r;
-        name = n;
+    public static void main(String[] args) {
+        displayGeometricObject(new Circle(1, "red", false));
+        displayGeometricObject(new Rectangle(1, 1, "black", true));
     }
-    void display() {
-        System.out.println(rollno + " - " + name + " - " + college);
-    }
-    public static void main(String arg[]) {
-        Student8 s1 = new Student8(111,"Thông");
-        Student8 s2 = new Student8(222,"Minh");
-        s1.display();
-        s2.display();
+    public static void displayGeometricObject(Geometric geometricObj){
+        System.out.println("Created on " + geometricObj.getDateCreated()+
+                ". Color is " +geometricObj.getColor());
     }
 }
